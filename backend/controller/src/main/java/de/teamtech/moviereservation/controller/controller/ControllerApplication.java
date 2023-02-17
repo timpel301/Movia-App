6cdd4e0dcaa5ccf12a53a2c1fe752062403cb253
timpel301 <tim.pel301@gmail.com>
2023-02-17 13:32:12 +0100

    cleaned up pom and code

--- backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
+++ backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
@@ -4,15 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.fabric8.kubernetes.api.model.Secret;
-import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.KubernetesClient;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,8 +19,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Base64;
-import java.util.Map;
 
 @RestController
 @SpringBootApplication
@@ -41,21 +32,13 @@ public class ControllerApplication {
         String name = jsonObj.getString("name");
         int numberOfTickets = jsonObj.getInt("numberOfTickets");
 
-        // Insert the data into the database
-		// Config config = new ConfigBuilder().build();
-		// KubernetesClient client = new DefaultKubernetesClient(config);
-		// Secret secret = client.secrets().inNamespace("teamtech-ns").withName("postgres-secret").get();
-
-		// Map<String, String> data = secret.getData();
-		// String username = new String(Base64.getDecoder().decode(data.get("username")));
-		// String password = new String(Base64.getDecoder().decode(data.get("password")));
-
-
+        // database connection
         try {
             Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://20.4.203.253:5432/postgres";
             Connection conn = DriverManager.getConnection(url, "root", "mypassword");
 
+            // database table
             DatabaseMetaData dbmd = conn.getMetaData();
             ResultSet rs = dbmd.getTables(null, null, "bookings", null);
             if (!rs.next()) {
@@ -65,6 +48,7 @@ public class ControllerApplication {
             stmt.executeUpdate(sql);
             }
 
+            // inpute data into database
             String sql = "INSERT INTO bookings (title, name, number_of_tickets) VALUES (?, ?, ?)";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, title);
@@ -72,6 +56,7 @@ public class ControllerApplication {
             pstmt.setInt(3, numberOfTickets);
             pstmt.executeUpdate();
 
+            // close connection
             conn.close();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -81,11 +66,11 @@ public class ControllerApplication {
             return new ResponseEntity<>("Error: database error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-
         // Return a response
         return new ResponseEntity<>("Booking created successfully!", HttpStatus.CREATED);
     }
 
+    // dummy get request
     @GetMapping("/alive")
     public String alive() {
         return "alive";
