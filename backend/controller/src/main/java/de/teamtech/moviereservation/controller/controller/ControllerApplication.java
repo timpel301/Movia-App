a72692c0d6efe92757c244fca77089455420e815
timpel301 <tim.pel301@gmail.com>
2023-02-17 11:01:12 +0100

    test db connection

--- backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
+++ backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
@@ -20,9 +20,12 @@ import org.springframework.http.HttpStatus;
 import org.json.JSONObject;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Base64;
 import java.util.Map;
 
@@ -39,7 +42,45 @@ public class ControllerApplication {
         int numberOfTickets = jsonObj.getInt("numberOfTickets");
 
         // Insert the data into the database
-        // ...
+		Config config = new ConfigBuilder().build();
+		KubernetesClient client = new DefaultKubernetesClient(config);
+		Secret secret = client.secrets().inNamespace("teamtech-ns").withName("postgres-secret").get();
+
+		Map<String, String> data = secret.getData();
+		String username = new String(Base64.getDecoder().decode(data.get("username")));
+		String password = new String(Base64.getDecoder().decode(data.get("password")));
+
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://20.4.203.253:5432/tech-db";
+            Connection conn = DriverManager.getConnection(url, username, password);
+
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet rs = dbmd.getTables(null, null, "bookings", null);
+            if (!rs.next()) {
+            // Table does not exist, create it
+            Statement stmt = conn.createStatement();
+            String sql = "CREATE TABLE bookings (id SERIAL PRIMARY KEY, title VARCHAR(255), name VARCHAR(255), number_of_tickets INTEGER)";
+            stmt.executeUpdate(sql);
+            }
+
+            String sql = "INSERT INTO bookings (title, name, number_of_tickets) VALUES (?, ?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, title);
+            pstmt.setString(2, name);
+            pstmt.setInt(3, numberOfTickets);
+            pstmt.executeUpdate();
+
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: database driver not found", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: database error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
 
         // Return a response
         return new ResponseEntity<>("Booking created successfully!", HttpStatus.CREATED);
@@ -49,44 +90,6 @@ public class ControllerApplication {
     public String alive() {
         return "alive";
     }
-
-        // Insert the data into the database
-		// Config config = new ConfigBuilder().build();
-		// KubernetesClient client = new DefaultKubernetesClient(config);
-		// Secret secret = client.secrets().inNamespace("my-namespace").withName("postgres-secret").get();
-
-		// Map<String, String> data = secret.getData();
-		// String username = new String(Base64.getDecoder().decode(data.get("username")));
-		// String password = new String(Base64.getDecoder().decode(data.get("password")));
-
-
-        // try {
-        //     Class.forName("org.postgresql.Driver");
-        //     String url = "jdbc:postgresql://"+ System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME");
-        //     Connection conn = DriverManager.getConnection(url, username, password);
-
-        //     String sql = "INSERT INTO bookings (title, name, number_of_tickets) VALUES (?, ?, ?)";
-        //     PreparedStatement pstmt = conn.prepareStatement(sql);
-        //     pstmt.setString(1, title);
-        //     pstmt.setString(2, name);
-        //     pstmt.setInt(3, numberOfTickets);
-        //     pstmt.executeUpdate();
-
-        //     conn.close();
-        // } catch (ClassNotFoundException e) {
-        //     e.printStackTrace();
-        //     return new ResponseEntity<>("Error: database driver not found", HttpStatus.INTERNAL_SERVER_ERROR);
-        // } catch (SQLException e) {
-        //     e.printStackTrace();
-        //     return new ResponseEntity<>("Error: database error", HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
-
-        // Return a response
-    
-
-	// public void run() {
-    //     SpringApplication.run(ControllerApplication.class);
-    // }
 
 
     public static void main(String[] args) {
--- backend/controller/src/test/java/de/teamtech/moviereservation/controller/controller/ControllerApplicationTests.java
+++ backend/controller/src/test/java/de/teamtech/moviereservation/controller/controller/ControllerApplicationTests.java
@@ -1,13 +1,36 @@
 package de.teamtech.moviereservation.controller.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import static org.junit.Assert.assertEquals;
+// import static org.mockito.Mockito.*;
 
-@SpringBootTest
-class ControllerApplicationTests {
+// import org.json.JSONObject;
+// import org.junit.Before;
+// import org.junit.Test;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 
-	@Test
-	void contextLoads() {
-	}
+// public class ControllerApplicationTest {
+//     private ControllerApplication controller;
+    
+//     @Before
+//     public void setup() {
+//         controller = new ControllerApplication();
+//     }
+    
+//     @Test
+//     public void testCreateBooking() {
+//         // Create a mock JSON payload
+//         JSONObject payload = new JSONObject();
+//         payload.put("title", "Mr.");
+//         payload.put("name", "John Doe");
+//         payload.put("numberOfTickets", 2);
+        
+//         // Call the createBooking method with the mock payload
+//         ResponseEntity<String> response = controller.createBooking(payload.toString());
+        
+//         // Check that the response has the correct status code and message
+//         assertEquals(HttpStatus.CREATED, response.getStatusCode());
+//         assertEquals("Booking created successfully!", response.getBody());
+//     }
+// }
 
-}
