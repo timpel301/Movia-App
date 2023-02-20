c3704b863a8bd478d9e2cbf7137d0d888ce6ca1a
timpel301 <tim.pel301@gmail.com>
2023-02-20 22:05:15 +0100

    again...

--- backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
+++ backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
@@ -19,6 +19,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.lang.System;
 
 @RestController
 @SpringBootApplication
