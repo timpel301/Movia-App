7e2ab580c03d0bb705b433319bad6991e982bc60
timpel301 <tim.pel301@gmail.com>
2023-02-21 09:26:21 +0100

    removed java.system.lang

--- backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
+++ backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
@@ -19,7 +19,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.lang.System;
 
 @RestController
 @SpringBootApplication
