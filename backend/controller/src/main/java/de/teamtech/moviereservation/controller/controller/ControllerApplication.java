42a170bd3e0cdd85c1a36b5afc4d8eac3b228406
timpel301 <tim.pel301@gmail.com>
2023-02-22 15:37:25 +0100

    prometheus

--- backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
+++ backend/controller/src/main/java/de/teamtech/moviereservation/controller/controller/ControllerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpStatus;
